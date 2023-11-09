fbdf3620834796bc0916eb1e47d2496bc17f7c05
vikramaditya <vikramaditya.singh2021@vitstudent.ac.in>
2023-11-09 17:09:55 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,122 @@
+const logotext = "Hello Cyberpunks";
+const meta = {
+    title: "Abhishek Ambasta Portfolio",
+    description: "I’m Abhishek Ambasta data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Abhishek Ambasta",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+};
+
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "I am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience. I like to read, I like to write; I like to think, I like to dream; I like to talk, I like to listen. I like to see the sunrise in the morning, I like to see the moonlight at night; I like to feel the music flowing on my face, I like to smell the wind coming from the ocean.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "Biratnagar , Nepal",
+        date: "2010",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Dhanbad , Jharkhand",
+        date: "2015",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Chennai , Tamil Nadu",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 60,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Web Design",
+        description: "Welcome to my web design services!.Your Vision, Our Expertise: Crafting Exceptional Web Experiences.",
+    },
+    {
+        title: "UI/UX Design",
+        description: "Welcome to my UI/UX design services!.Crafting User-Friendly Digital Experiences.",
+    },
+    {
+        title: "App Design",
+        description: "Welcome to my app design services!.Elevating Mobile App Design.",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://picsum.photos/400/400/?grayscale",
+        description: "HTML Project.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/400/?grayscale",
+        description: "Css Project",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/400/?grayscale",
+        description: "Java Script Project",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abhishek.ambasta2021@vitstudent.ac.in",
+    YOUR_FONE: "7070504379",
+    description: "Send me a message for any query , I am here for any kind of Tech Help ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
